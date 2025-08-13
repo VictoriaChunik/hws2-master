a465426e95253779530833e37ad3747484c59264
VictoriaChunik <vikcoding24@gmail.com>
2025-08-13 10:04:38 +0300

    new 10 task

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,26 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export type ThemeState = {
+    themeId: number  // должно совпадать с initState
+}
+
+export const themeReducer = (state = initState, action: ChangeThemeIdAction): ThemeState => { // fix any
+    switch (action.type) {  // дописать
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id,
+            }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChangeThemeIdAction => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export type ChangeThemeIdAction = {
+    type: 'SET_THEME_ID'
+    id: number
+}
